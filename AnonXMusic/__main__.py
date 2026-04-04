0b9586e5defd2fea7341c41d5ae2e28ad90847b5
abhsjnsism-gabhirajlitch <abhsjnsism@gmail.com>
2026-04-04 22:36:26 +0530

    Update __main__.py
    Signed-off-by: abhsjnsism-gabhirajlitch <abhsjnsism@gmail.com>

--- AnonXMusic/__main__.py
+++ AnonXMusic/__main__.py
@@ -1,5 +1,8 @@
 import asyncio
 import importlib
+import os
+from flask import Flask
+from threading import Thread
 
 from pyrogram import idle
 from pytgcalls.exceptions import NoActiveGroupCall
@@ -12,6 +15,24 @@ from AnonXMusic.plugins import ALL_MODULES
 from AnonXMusic.utils.database import get_banned_users, get_gbanned
 from config import BANNED_USERS
 
+# --- Render Port Binding (Flask Server) ---
+web_app = Flask(__name__)
+
+@web_app.route('/')
+def home():
+    return "Bot is running!"
+
+def run_web_server():
+    # Render default port 10000 use karta hai agar PORT env na mile
+    port = int(os.environ.get("PORT", 8080))
+    web_app.run(host="0.0.0.0", port=port)
+
+def keep_alive():
+    t = Thread(target=run_web_server)
+    t.daemon = True
+    t.start()
+# -----------------------------------------
+
 async def init():
     if (
         not config.STRING1
@@ -22,7 +43,9 @@ async def init():
     ):
         LOGGER(__name__).error("Assistant client variables not defined, exiting...")
         exit()
+    
     await sudo()
+    
     try:
         users = await get_gbanned()
         for user_id in users:
@@ -32,23 +55,34 @@ async def init():
             BANNED_USERS.add(user_id)
     except:
         pass
+
     await app.start()
+    
     for all_module in ALL_MODULES:
         importlib.import_module("AnonXMusic.plugins" + all_module)
+    
     LOGGER("AnonXMusic.plugins").info("Successfully Imported Modules...")
+    
     await userbot.start()
     await Anony.start()
+    
     try:
         await Anony.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
     except NoActiveGroupCall:
         LOGGER("AnonXMusic").error(
-            "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
+            "Please turn on the videochat of your log group\\channel.\\n\\nStopping Bot..."
         )
-        exit()
+        # Yahan exit() ki jagah hum bot ko chalne denge taaki web server down na ho
+        pass
     except:
         pass
 
     await Anony.decorators()
+    
+    # Flask server yahan start hoga
+    keep_alive()
+    LOGGER("AnonXMusic").info("Web Server Started for Render Port Binding...")
+
     await idle()
     await app.stop()
     LOGGER("AnonXMusic").info("Stopping AnonX Music Bot...")
